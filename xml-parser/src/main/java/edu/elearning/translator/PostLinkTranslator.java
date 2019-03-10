748d214fd7f89c9777775a693b7fc02426b9b43d
Hanuman Prasad <hanuman-prasad@outlook.in>
2019-03-10 09:55:07 +0530

    post link enup type mapped

--- xml-parser/src/main/java/edu/elearning/translator/PostLinkTranslator.java
+++ xml-parser/src/main/java/edu/elearning/translator/PostLinkTranslator.java
@@ -1,10 +1,12 @@
 package edu.elearning.translator;
 
+import edu.elearning.se.LinkType;
 import edu.elearning.se.PostLink;
 
 import java.util.Map;
 import java.util.logging.Logger;
 
+import static edu.elearning.translator.TranslatorUtils.getEnumFromEnumIndex;
 import static edu.elearning.translator.TranslatorUtils.getLocalDate;
 import static edu.elearning.translator.TranslatorUtils.getValueFromInputMap;
 
@@ -19,7 +21,7 @@ public class PostLinkTranslator implements Translator<PostLink> {
 
         PostLink postLink = PostLink.builder()
                 .id(getValueFromInputMap(map, "id"))
-                .linkTypeId(getValueFromInputMap(map, "linktypeid"))
+                .linkType(getEnumFromEnumIndex(LinkType.class,getValueFromInputMap(map, "linktypeid")))
                 .postId(getValueFromInputMap(map, "postid"))
                 .relatedPostId(getValueFromInputMap(map, "relatedpostid"))
                 .creationDate(getLocalDate(map, "creationdate"))
