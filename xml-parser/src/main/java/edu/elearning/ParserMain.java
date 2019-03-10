b93ce81909e428ee550a8d73108d02cf0fe5c2b4
Hanuman Prasad <hanuman-prasad@outlook.in>
2019-03-10 10:30:50 +0530

    continue if website type is not defined

--- xml-parser/src/main/java/edu/elearning/ParserMain.java
+++ xml-parser/src/main/java/edu/elearning/ParserMain.java
@@ -2,6 +2,7 @@ package edu.elearning;
 
 import edu.elearning.regex.StackExchangeRegex;
 import edu.elearning.se.AsteriModel;
+import edu.elearning.se.UserWebsite;
 import edu.elearning.translator.TranslationException;
 import edu.elearning.translator.Translator;
 import edu.elearning.translator.TranslatorRegistry;
@@ -14,18 +15,25 @@ import java.io.FileNotFoundException;
 import java.util.*;
 import java.util.logging.Logger;
 
+import static edu.elearning.translator.TranslatorUtils.getEnumFromString;
+
 public class ParserMain {
 
     private static final Logger LOG = Logger.getLogger("ParserMain");
 
     public static void main(String[] args) throws FileNotFoundException {
 
-        List<File> files = XmlFileHelper.getAllFilesForFolder("D:/stackexchange/hardware/");
+        List<File> files = XmlFileHelper.getAllFilesForFolder("D:/stackexchange/devops/");
         Map<String, List<AsteriModel>> modelMap = new HashMap<>();
 
         for (File file : files) {
 
             final String website_name = file.getParentFile().getName();
+
+            if(!isWebsiteDefined(website_name)){
+                LOG.warning("Website type [" + website_name + "] is not defined in UserWebsite class!");
+                continue;
+            }
 
             String name = file.getName();
 
@@ -67,6 +75,10 @@ public class ParserMain {
         }
 
         LOG.info("Number of file parsed - " + modelMap.size());
+    }
+
+    private static boolean isWebsiteDefined(String website_name) {
+        return getEnumFromString(UserWebsite.class, website_name) != null;
     }
 
 
