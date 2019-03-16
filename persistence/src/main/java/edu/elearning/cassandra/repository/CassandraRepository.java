9930b02fd1f82ce652077bebdb177afacf791924
Hanuman Prasad <hanuman-prasad@outlook.in>
2019-03-16 18:12:25 +0530

    adding persist and query functionality in cassandra

---
+++ persistence/src/main/java/edu/elearning/cassandra/repository/CassandraRepository.java
@@ -0,0 +1,10 @@
+package edu.elearning.cassandra.repository;
+
+import edu.elearning.se.AsteriModel;
+
+public class CassandraRepository implements Repository {
+    @Override
+    public void save(Class<? extends AsteriModel> typeClass, AsteriModel model) {
+
+    }
+}
