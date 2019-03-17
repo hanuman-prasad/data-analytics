51f80471535111cf0121db001c72b5f37b80be68
Hanuman Prasad <hanuman-prasad@outlook.in>
2019-03-17 17:43:07 +0530

    projection defined for entities

--- persistence/src/main/java/edu/elearning/cassandra/projection/EntityRegistry.java
+++ persistence/src/main/java/edu/elearning/cassandra/projection/EntityRegistry.java
@@ -10,6 +10,11 @@ public interface EntityRegistry {
 
     Map<String, List<String>> projections =
             ImmutableMap.<String, List<String>>builder()
+                    .put("Badge", ImmutableList.of("id", "name", "userId"))
+                    .put("Comment", ImmutableList.of("id", "postId", "userId"))
+                    .put("Post", ImmutableList.of("id", "tags", "ownerUserId"))
+                    .put("PostLink", ImmutableList.of("id", "relatedPostId", "postId"))
                     .put("User", ImmutableList.of("id", "upVotes", "views"))
+                    .put("Vote", ImmutableList.of("id", "postId"))
                     .build();
 }
