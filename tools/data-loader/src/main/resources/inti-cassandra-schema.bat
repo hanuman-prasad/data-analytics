8eb269219395dc4473cc13d1129200fca37a8c46
Hanuman Prasad <hanuman-prasad@outlook.in>
2019-10-05 20:15:37 +0530

    adding cassandra schema intialization script

---
+++ tools/data-loader/src/main/resources/inti-cassandra-schema.bat
@@ -0,0 +1,27 @@
+@echo off
+set currentDir=%cd%
+echo Current directory : %currentDir%
+
+set workingDir=%userprofile%\data-analytics
+echo Wroking dirctory : %workingDir%
+mkdir %workingDir%
+cd /d %workingDir%
+echo Downloading apache cassandra
+curl http://mirrors.estointernet.in/apache/cassandra/3.11.4/apache-cassandra-3.11.4-bin.tar.gz -o cassandra.tar.gz
+
+echo Extracting to %workingDir%
+tar -x -f cassandra.tar.gz
+
+echo Starting cassandra, wait for 60 sec..
+start apache-cassandra-3.11.4\bin\cassandra.bat
+timeout /t 60 > nul
+
+cd /d %currentDir%
+set schemaFilePath= ../../../../../persistence/src/main/resources/cassandra/schema/
+set schemaFileName=entities.cql
+
+cd /d %schemaFilePath%
+set cqlCmd=cqlsh -e "SOURCE '%cd%/%schemaFileName%'"
+echo importing schema using : %cqlCmd%
+%cqlCmd%
+cd /d %currentDir%
